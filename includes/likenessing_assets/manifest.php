3b3fe1dd2c7743d816e460dcf176b26a9d7a9f32
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-16 13:15:31 -0700

    Add Likenessing raster asset manifest

---
+++ includes/likenessing_assets/manifest.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'hero' => ['type' => 'image/webp', 'chunks' => ['chunk-01.php','chunk-02.php']],
+  'premise' => ['type' => 'image/webp', 'chunks' => ['chunk-03.php','chunk-04.php']],
+  'cast' => ['type' => 'image/webp', 'chunks' => ['chunk-05.php','chunk-06.php']],
+  'episodes' => ['type' => 'image/webp', 'chunks' => ['chunk-07.php']],
+  'newsletter' => ['type' => 'image/webp', 'chunks' => ['chunk-08.php','chunk-09.php']],
+  'logo' => ['type' => 'image/webp', 'chunks' => ['chunk-10.php']],
+  'icons' => ['type' => 'image/webp', 'chunks' => ['chunk-11.php']],
+  'favicon' => ['type' => 'image/png', 'chunks' => ['chunk-12.php','chunk-13.php']],
+];
