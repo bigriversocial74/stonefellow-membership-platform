4414348374eeb735bef43de1aa82c01d8f5f98e2
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-16 13:27:14 -0700

    Finalize embedded raster asset manifest

--- includes/likenessing_assets/manifest.php
+++ includes/likenessing_assets/manifest.php
@@ -4,8 +4,6 @@ return [
   'premise' => ['type' => 'image/webp', 'chunks' => ['chunk-03.php','chunk-04.php']],
   'cast' => ['type' => 'image/webp', 'chunks' => ['chunk-05.php','chunk-06.php']],
   'episodes' => ['type' => 'image/webp', 'chunks' => ['e-01.php','e-02.php']],
-  'newsletter' => ['type' => 'image/webp', 'chunks' => ['n-01.php','n-02.php','n-03.php','n-04.php']],
   'logo' => ['type' => 'image/webp', 'chunks' => ['l-01.php']],
   'icons' => ['type' => 'image/webp', 'chunks' => ['i-01.php','i-02.php']],
-  'favicon' => ['type' => 'image/png', 'chunks' => ['f-01.php','f-02.php','f-03.php']],
 ];
